fd8cbceb50f4765fc10959626aa1578490c5e6f5
jemel119 <jeremyhenryatohengbe@gmail.com>
2026-03-12 22:33:29 -0400

    Add splash screen with automated home screen navigation

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.restaurant, size: 80),
+            SizedBox(height: 20),
+            Text(
+              "Campus Food Finder",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
