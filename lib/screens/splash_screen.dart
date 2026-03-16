d4f267c08d4ae956159611147218881112c159c4
jemel119 <jeremyhenryatohengbe@gmail.com>
2026-03-16 16:04:19 -0400

    Add recommendation screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,24 +13,30 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, '/home');
     });
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: const [
+
             Icon(Icons.restaurant, size: 80),
+
             SizedBox(height: 20),
+
             Text(
               "Campus Food Finder",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            )
+              style: TextStyle(fontSize: 24),
+            ),
+
           ],
         ),
       ),
