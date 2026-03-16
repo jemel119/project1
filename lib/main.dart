d06802917c2187e58793f15489097847af410000
jemel119 <jeremyhenryatohengbe@gmail.com>
2026-03-16 15:14:21 -0400

    Add light and dark mode for improved accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,33 @@ class CampusFoodFinderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Campus Food Finder',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/foods': (context) => const FoodListScreen(),
-        '/budget': (context) => const BudgetScreen(),
-        '/add_food': (context) => const AddFoodScreen(),
-        '/expenses': (context) => const ExpenseListScreen(),
-        '/add_expense': (context) => const AddExpenseScreen(),
-        '/recommend': (context) => const RecommendationScreen(),
-      },
-    );
+  debugShowCheckedModeBanner: false,
+
+  title: "Student Food App",
+
+  theme: ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.green,
+    scaffoldBackgroundColor: Colors.grey[100],
+  ),
+
+  darkTheme: ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.green,
+  ),
+
+  themeMode: ThemeMode.system,
+
+  initialRoute: '/',
+  routes: {
+    '/': (context) => const SplashScreen(),
+    '/home': (context) => const HomeScreen(),
+    '/food_list': (context) => const FoodListScreen(),
+    '/add_food': (context) => const AddFoodScreen(),
+    '/expenses': (context) => const ExpenseListScreen(),
+    '/add_expense': (context) => const AddExpenseScreen(),
+    '/recommend': (context) => const RecommendationScreen(),
+  },
+);
   }
 }
